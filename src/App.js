8e387389a315e09e2f4e181cf47363e16305e3c0
Yusuf <baliyusuf675@gmail.com>
2023-03-02 15:02:11 +0300

    end

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
+import Find from "./components/find/Find";
+import Driver from "./components/driver/Driver";
+import Luxury from "./components/luxury/Luxury";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Find />
+      <Driver />
+      <Luxury />
+      <Footer />
     </div>
   );
 }
